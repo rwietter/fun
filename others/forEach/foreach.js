bd93343991e3d9b9a54be101d8c0b06556919490
Maurício Witter <mwitter@protonmail.com>
2020-06-21 00:17:12 -0300

    fix: push errors for names

--- others/forEach/foreach.js
+++ others/forEach/foreach.js
@@ -1,33 +1,47 @@
 const errorsMessages = [
-  { zip_code: { id: 1, message: 'Cep incorreto' } },
-  { address: { id: 2, message: 'Endereço incorreto' } },
-  { number: { id: 3, message: 'Número incorreto' } },
-  { test: { id: 4, message: 'Identificação do MEC' } },
-  { resources: { id: 5, message: 'Tipos de recursos' } },
-  { teste: { id: 6, message: 'Hello' } }
-]
+  { zip_code: { id: 1, message: "Cep incorreto" } },
+  { address: { id: 2, message: "Endereço incorreto" } },
+  { number: { id: 3, message: "Número incorreto" } },
+  { test: { id: 4, message: "Identificação do MEC" } },
+  { resources: { id: 5, message: "Tipos de recursos" } },
+  { teste: { id: 6, message: "Hello" } },
+];
 
-const typeErrors = ['number', 'resources', 'zip_code']
+const typeErrors = ["number", "resources", "zip_code"];
 
 const includesObject = () => {
-  let newErrors = []
+  let newErrors = [];
   errorsMessages.map((object, index) => {
-    let objectKey = Object.keys(errorsMessages[index])
-    typeErrors.filter(error => error.includes(objectKey) ? newErrors.push(object) : null)
-  }, [])
+    let objectKey = Object.keys(errorsMessages[index]);
+    typeErrors.filter((error) =>
+      error.includes(objectKey) ? newErrors.push(object) : null
+    );
+  }, []);
   return newErrors;
-}
+};
 
 const pushError = () => {
-  const errorMessage = []
+  const errorMessage = [];
   includesObject().forEach((element, index) => {
-    errorMessage.push(element)
-    console.log(element);
-  })
+    const nameErrorMessage = Object.keys(element);
+    errorMessage.push(element[nameErrorMessage]);
+  });
   return errorMessage;
-}
+};
 
 console.log(pushError());
+
+
+/*
+const pushError = () => {
+  const errorMessage = [];
+  includesObject().forEach((element, index) => {
+    errorMessage.push(element);
+  });
+  return errorMessage;
+};
+*/
+
 
 /*
 if error not includes in array, so insert this errors
