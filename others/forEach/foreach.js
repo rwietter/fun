eddb803895f1b97c7af34ca75a3bd91e6b1135f1
Maurício Witter <mwitter@protonmail.com>
2020-06-20 23:53:22 -0300

    add: solve error messages

--- others/forEach/foreach.js
+++ others/forEach/foreach.js
@@ -1,22 +1,48 @@
 const errorsMessages = [
   { zip_code: { id: 1, message: 'Cep incorreto' } },
   { address: { id: 2, message: 'Endereço incorreto' } },
-  { number: { id: 3, message: 'Número incorreto' } }
+  { number: { id: 3, message: 'Número incorreto' } },
+  { test: { id: 4, message: 'Identificação do MEC' } },
+  { resources: { id: 5, message: 'Tipos de recursos' } },
+  { teste: { id: 6, message: 'Hello' } }
 ]
 
-const errors = ['zip_code', 'address', 'number']
-const erro = [];
+const typeErrors = ['number', 'resources', 'zip_code']
 
-errors.forEach((el, index) => {
-  erro.push(errorsMessages[index][el])
-})
+const includesObject = () => {
+  let newErrors = []
+  errorsMessages.map((object, index) => {
+    let objectKey = Object.keys(errorsMessages[index])
+    typeErrors.filter(error => error.includes(objectKey) ? newErrors.push(object) : null)
+  }, [])
+  return newErrors;
+}
 
-console.log(erro);
+const pushError = () => {
+  const errorMessage = []
+  includesObject().forEach((element, index) => {
+    errorMessage.push(element)
+    console.log(element);
+  })
+  return errorMessage;
+}
 
+console.log(pushError());
 
-// output
-[
-  { id: 1, message: 'Cep incorreto' },
-  { id: 2, message: 'Endereço incorreto' },
-  { id: 3, message: 'Número incorreto' }
-]
+/*
+if error not includes in array, so insert this errors
+
+function includesObject() {
+  let arr = []
+  errorsMessages.map((el, index) => {
+    let objectKey = Object.keys(errorsMessages[index]);
+    if (!errors.includes(objectKey[0])) {
+      arr.push(objectKey)
+      return;
+    }
+    return;
+  }, [])
+  const newArr = arr.map(el => el[0])
+  return [...errors, ...newArr];
+}
+*/
