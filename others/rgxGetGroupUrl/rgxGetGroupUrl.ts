90d95ca974c255a6fee61a224ebd3ef1a4d3d602
Maurício Witter <mwitter@protonmail.com>
2020-07-05 01:03:36 -0300

    add: quiz and others algorithms

--- others/rgxGetGroupUrl/rgxGetGroupUrl.ts
+++ others/rgxGetGroupUrl/rgxGetGroupUrl.ts
@@ -1,6 +1,6 @@
 let url = "primeiro.segundo.meudominio.com.br";
 
-let rgxcp = /^(\w+).(\w+).(\w*.com.br)$/i;
+let rgxcp = /^(\w+).(\w+).(\w*)$/i;
 let match = url.match(rgxcp)!;
 
 console.log(`${match[1]}.${match[3]}`); // primeiro.meudominio.com.br
