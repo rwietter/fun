fe388553eb357b69052263c17da1a8de496e2683
rwietter <rwietter@zohomail.com>
2023-09-24 12:43:56 -0300

    add: elixir prize draw

---
+++ Playground/elixir/prizedraw/mix.exs
@@ -0,0 +1,30 @@
+defmodule Prizedraw.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :Main,
+      version: "0.1.0",
+      elixir: "~> 1.15",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:file_system, "~> 0.2.10"},
+      {:mix_test_watch, "~> 0.9", only: :dev}
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+end
